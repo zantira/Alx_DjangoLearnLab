b1d6c87e184b24cfc8e47eb796b1d63e5dd786ab
Chris Zantira <chrizantira@gmail.com>
2024-11-05 08:05:46 +0000

    update book

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,9 +3,12 @@ from .models import Book
 # Register your models here.
 admin.site.register(Book)
 
+# customized book Admin interface
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_year')
     search_fields = ('title', 'author')
     list_filter = ['publication_year']
+    
 # register BookAdmin
 admin.site.register(Book, BookAdmin)
