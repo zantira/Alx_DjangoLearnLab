4f79a49878a64af08630323386a7cb56706d2862
Chris Zantira <chrizantira@gmail.com>
2024-11-08 21:00:28 +0000

    update relationship_app

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -14,7 +14,7 @@ author = Author.objects.get(name='Author name')
 books = Book.objects.filter(author=author)
 
 #list all books in the library
-library = Library.objects.get(name='library_name')
+library = Library.objects.get(name="name of library")
 all_books = library.books.all()
 
 #retrieve the name of the librarian
