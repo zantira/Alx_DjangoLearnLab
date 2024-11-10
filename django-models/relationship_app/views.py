0a8c81d2aa1f817faecbde05c690ddd67758ebd3
Chris Zantira <chrizantira@gmail.com>
2024-11-10 17:40:07 +0000

    update relationship

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def book_list(request):
