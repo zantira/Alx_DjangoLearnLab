cca27eeb2f2bdf684cff36ef8611269d8ef955c2
Chris Zantira <chrizantira@gmail.com>
2024-11-10 17:35:43 +0000

    update realtionship_app/library_detail.html

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from models import Book, Library
+from models import Book
+from . models import Library
 from django.views.generic import DetailView
 
 # Create your views here.
@@ -13,17 +14,20 @@ class BookDetailtView(DetailView):
     model = Book
     template_name = 'list_books.html'
     
-    def get_book_details(self, **kwargs):
-        context = super().get_book_details(**kwargs)
+    def get_book_details(self, request):
+        context = super().get_book_details(request)
         book = self.get_objects()
         context['average_rating'] = book.get_average_rating()
-        
+        return HttpResponse(request, 'relationship_app/list_books.html', context)
+    
         
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     
-    def get_library_details(self, **kwargs):
-        context = super().get_library_details(**kwargs)
+    
+    def get_library_details(self, request):
+        context = super().get_library_details(request)
         library = self.get_objects()
         context['library_details'] = library.get_library_details()
+        return HttpResponse(request, 'relationship_app/library_detail.html', context)
