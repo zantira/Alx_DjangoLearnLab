4a7052b2bfefe6fe89e74035ccef0986516170b5
Chris Zantira <chrizantira@gmail.com>
2024-11-11 15:04:35 +0000

    add user registration/login/logout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 from views import LibraryDetailView
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+
 #add urls
 urlpatterns = [
     path('book/', views.book_list, name='book' ),
     path('bookDetails/', views.BookDetailtView, name='bookDetails'),
-    path('libraryDetails/', LibraryDetailView.as_view(), name='libraryDetails')
+    path('libraryDetails/', LibraryDetailView.as_view(), name='libraryDetails'),
+    path('login/', LoginView.as_view(template_name='login.html', name='login')),
+    path('logout/', LogoutView.as_view(template_name='logout.html', name='logout')),
+    
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -31,3 +31,20 @@ class LibraryDetailView(DetailView):
         library = self.get_objects()
         context['library_details'] = library.get_library_details()
         return HttpResponse(request, 'relationship_app/library_detail.html', context)
+    
+    
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login
+
+#Create a view that handles form submission and redirects users upon successful registration.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+        return redirect(request, 'register,html', {'form': form})
