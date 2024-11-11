f62d9fd2945851b8a4416be33905d1d9c8e1ac0d
Chris Zantira <chrizantira@gmail.com>
2024-11-11 18:06:48 +0000

    delete forms.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -34,7 +34,7 @@ class LibraryDetailView(DetailView):
     
     
 from django.shortcuts import render, redirect
-from .forms import RegistrationForm
+
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -42,7 +42,7 @@ from django.contrib.auth.models import User
 #Create a view that handles form submission and redirects users upon successful registration.
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
