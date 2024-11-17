62b0ed9ce3314602b001c2768ebeb40ac7a7c928
Chris Zantira <chrizantira@gmail.com>
2024-11-17 16:35:17 +0000

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +11,42 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} {self.author}, {self.publication_year}"
+    
+    
+#define custom user manager
+class CustomUserManger(BaseUserManager):
+    
+    def creat_user(self, email, password, profile_photo=None, **extra_fields):
+        if not email:
+            raise ValueError(_('You must add an email address'))
+        email = self.normalize_email(email)
+        user  = self.model(email=email, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    #create superuser
+    def create_superuser(self, email, password, profile_photo=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        #validate superuser
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser=True')
+        
+        return self.create_user(email, password, profile_photo, **extra_fields)
+
+#create custom user model by extending AbstractUser
+class CustomUser(AbstractUser):
+    email          =  models.EmailField(unique=True)
+    profile_photo  =  models.ImageField(blank=True, null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = CustomUserManger()
+    
+    
+    def __str__(self):
+        return self.email
