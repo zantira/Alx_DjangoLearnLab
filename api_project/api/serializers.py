d530071033174b70463ef9dc02470d96f624481c
Chris Zantira <chrizantira@gmail.com>
2024-11-23 18:04:53 +0000

    corrected typos

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from api.models import Book
 # Serializers definition
-class BookSeliazer(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import BookSeliazer
+from .serializers import BookSerializer
 from .models import Book
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookSeliazer
+    serializer_class = BookSerializer
