379c83926e425663c479dc648a0a84011fd82805
Chris Zantira <chrizantira@gmail.com>
2024-11-30 18:30:27 +0000

    add permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -11,3 +13,10 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    
+class BookView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "Hello, authenticated user!"})
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 3rd party apps
+    # Third-Party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    # local apps
     'api',
 ]
 
@@ -126,3 +128,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
